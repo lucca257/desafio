b16b0d795e695a4eaf71b4eb46f475371317982e
pedro lucca <pedrolucca257@gmail.com>
2021-08-08 12:43:27 -0300

    test: deve criar carteira para o usuário

---
+++ tests/Unit/CarteiraTest.php
@@ -0,0 +1,22 @@
+<?php
+
+
+use App\Domain\carteira\DataTransferObjects\CarteiraData;
+use Tests\TestCase;
+
+class CarteiraTest extends TestCase
+{
+    public function test_deve_criar_uma_carteira_para_o_usuario()
+    {
+        $action = resolve(\App\Domain\carteira\Actions\CriarCarteiraAction::class);
+        $carteiraData = new CarteiraData(
+            usuario_id: 1,
+            saldo: 9999
+        );
+        $action->execute($carteiraData);
+        $this->assertDatabaseHas('carteiras', [
+            "usuario_id" => $carteiraData->usuario_id,
+            "saldo" => $carteiraData->saldo
+        ]);
+    }
+}
