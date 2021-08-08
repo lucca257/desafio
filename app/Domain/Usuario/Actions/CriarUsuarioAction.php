d2b078a8881386b0d6baed57876e147a8d23b063
pedro lucca <pedrolucca257@gmail.com>
2021-08-08 13:09:30 -0300

    feat: criando carteira ao criar usuário

--- app/Domain/Usuario/Actions/CriarUsuarioAction.php
+++ app/Domain/Usuario/Actions/CriarUsuarioAction.php
@@ -4,6 +4,8 @@
 namespace App\Domain\Usuario\Actions;
 
 
+use App\Domain\carteira\Actions\CriarCarteiraAction;
+use App\Domain\carteira\DataTransferObjects\CarteiraData;
 use App\Domain\Usuario\DataTransferObjects\UsuarioData;
 use App\Domain\Usuario\Exception\CpfCnpjException;
 use App\Domain\Usuario\Models\Usuario;
@@ -16,11 +18,12 @@ use App\Support\CpfCnpjValidacao;
 final class CriarUsuarioAction
 {
     /**
-     * CadastrarUsuarioAction constructor.
+     * CriarUsuarioAction constructor.
      * @param Usuario $usuario
      * @param CpfCnpjValidacao $cpfCnpjValidacao
+     * @param CriarCarteiraAction $criarCarteiraAction
      */
-    public function __construct(private Usuario $usuario, private CpfCnpjValidacao $cpfCnpjValidacao) {}
+    public function __construct(private Usuario $usuario, private CpfCnpjValidacao $cpfCnpjValidacao, private CriarCarteiraAction $criarCarteiraAction) {}
 
     /**
      * @param UsuarioData $usuarioData
@@ -30,13 +33,18 @@ final class CriarUsuarioAction
     public function execute(UsuarioData $usuarioData): Usuario
     {
         $usuarioData->cpf_cnpj = $this->validarCpfCnpj($usuarioData->cpf_cnpj);
-        return $this->usuario->create([
+        $usuarioCriado = $this->usuario->create([
             "nome" => $usuarioData->nome,
             "email" => $usuarioData->email,
             "password" => $usuarioData->password,
             "cpf_cnpj" => $usuarioData->cpf_cnpj,
             "tipo_usuario_id" => $usuarioData->tipo_id
         ]);
+        $this->criarCarteiraAction->execute(new CarteiraData(
+            usuario_id: $usuarioCriado->id,
+            saldo: 0
+        ));
+        return $usuarioCriado;
     }
 
     /**
