c62de041df18f8c5d15493080d79001475562517
pedro lucca <pedrolucca257@gmail.com>
2021-08-08 15:30:06 -0300

    feat: saldo da carteira do usuário

---
+++ app/Domain/carteira/Actions/SaldoCarteiraAction.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Domain\carteira\Actions;
+
+
+use App\Domain\carteira\DataTransferObjects\CarteiraData;
+use App\Domain\carteira\Models\Carteira;
+
+class SaldoCarteiraAction
+{
+    public function __construct(private Carteira $carteira){}
+
+    public function execute(int $usuario_id): float
+    {
+        return $this->carteira->where("usuario_id", $usuario_id)->first()->saldo ?? 0;
+    }
+}
