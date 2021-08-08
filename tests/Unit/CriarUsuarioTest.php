d3d4d45871eb63071fae77305b0104af5609214c
pedro lucca <pedrolucca257@gmail.com>
2021-08-08 12:44:08 -0300

    refactor: adicionando named arguments no mock de usuário test

--- tests/Unit/CriarUsuarioTest.php
+++ tests/Unit/CriarUsuarioTest.php
@@ -15,7 +15,13 @@ class CriarUsuarioTest extends TestCase
     public function test_deve_criar_um_usuario()
     {
         $action = resolve(CadastrarUsuarioAction::class);
-        $usuarioData = new UsuarioData("pedro lucca leonardo de almeida","13864107725","pedrolucca257@gmail.com",1,"any_password");
+        $usuarioData = new UsuarioData(
+            nome : "pedro lucca leonardo de almeida",
+            cpf_cnpj : "13864107725",
+            email : "pedrolucca257@gmail.com",
+            tipo_id : 1,
+            password : "any_password"
+        );
         $action->execute($usuarioData);
         $this->assertDatabaseHas('usuarios', [
             "nome" => $usuarioData->nome,
