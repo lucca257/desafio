ee8081e267e7f94dbf0ba384982045be4340a0f2
pedro lucca <pedrolucca257@gmail.com>
2021-08-08 12:47:11 -0300

    feat: modelo de carteira

---
+++ app/Domain/carteira/Models/Carteira.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Domain\carteira\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carteira extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+}
