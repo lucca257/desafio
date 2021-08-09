1062b153fa0253d4ed4d6aea59f538f6ed6aae76
pedro lucca <pedrolucca257@gmail.com>
2021-08-09 16:44:31 -0300

    feat: factory para testar dados

---
+++ database/factories/Domain/Carteira/models/CarteiraFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories\Domain\Carteira\models;
+
+use App\Domain\carteira\Models\Carteira;
+use App\Domain\Usuario\Models\Usuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CarteiraFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Carteira::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $usuarios = Usuario::all()->count();
+        return [
+            "usuario_id" =>$this->faker->unique()->numberBetween(1, $usuarios) ,
+            "saldo" => mt_rand(1,9999)
+        ];
+    }
+}
