62dc86e730d744112761f9d8f7de562d8a0e7d36
pedro lucca <pedrolucca257@gmail.com>
2021-08-09 16:43:20 -0300

    feat: factory para testar dados

---
+++ database/factories/Domain/Usuario/models/UsuarioFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories\Domain\Usuario\models;
+
+use App\Domain\Usuario\Models\Usuario;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UsuarioFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Usuario::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            "nome" => $this->faker->name,
+            "email" => $this->faker->email,
+            "password" => $this->faker->password,
+            "cpf_cnpj" => $this->faker->randomNumber(),
+            "tipo_usuario_id" => mt_rand(1,2)
+        ];
+    }
+}
