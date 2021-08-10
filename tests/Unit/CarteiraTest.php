bab821ac881f7c16ce2b5cc796b02b47781a01a3
pedro lucca <pedrolucca257@gmail.com>
2021-08-09 22:35:03 -0300

    refactor: movendo teste para classe correta

--- tests/Unit/CarteiraTest.php
+++ tests/Unit/CarteiraTest.php
@@ -3,9 +3,6 @@
 
 use App\Domain\carteira\DataTransferObjects\CarteiraData;
 use App\Domain\carteira\Models\Carteira;
-use App\Domain\Transferencia\Exception\SaldoInsuficiente;
-use App\Domain\Transferencia\Exception\TipoContaNaoPermitida;
-use App\Domain\Transferencia\Models\Transferencia;
 use App\Domain\Usuario\Models\Usuario;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -37,23 +34,5 @@ class CarteiraTest extends TestCase
         $this->assertNotSame(0,$saldoUsuario);
     }
 
-    /**
-     * @throws SaldoInsuficiente
-     * @throws TipoContaNaoPermitida
-     */
-    public function test_deve_ocorrer_uma_exception_ao_processar_transferencia_quando_usuario_for_do_tipo_lojista()
-    {
-        $this->withoutExceptionHandling();
-        $this->expectException(TipoContaNaoPermitida::class);
-        $this->expectExceptionMessage("Esta conta não pode realizar transferência");
-        $usuarios = Usuario::factory(2)->usuarioLojista()->create();
-        Carteira::factory(2)->create();
-        $action = resolve(\App\Domain\Transferencia\Actions\ProcessarTransferenciaAction::class);
-        $transferenciaMock = new \App\Domain\Transferencia\DataTransferObjects\TransferenciaData(
-            usuario_origem:  $usuarios->first()->id,
-            usuario_destino: $usuarios->last()->id,
-            valor: 99
-        );
-        $action->execute($transferenciaMock);
-    }
+
 }
--- tests/Unit/TransferenciaTest.php
+++ tests/Unit/TransferenciaTest.php
@@ -2,6 +2,8 @@
 
 
 use App\Domain\Transferencia\Actions\CriarTransferenciaAction;
+use App\Domain\Transferencia\Exception\SaldoInsuficiente;
+use App\Domain\Transferencia\Exception\TipoContaNaoPermitida;
 use App\Domain\Usuario\Models\Usuario;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -26,5 +28,25 @@ class TransferenciaTest extends TestCase
             "usuario_destino" => $usuarios->last()->id,
             "valor" => 123,
         ]);
+    }
+
+    /**
+     * @throws SaldoInsuficiente
+     * @throws TipoContaNaoPermitida
+     */
+    public function test_deve_ocorrer_uma_exception_ao_processar_transferencia_quando_usuario_for_do_tipo_lojista()
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException(TipoContaNaoPermitida::class);
+        $this->expectExceptionMessage("Esta conta não pode realizar transferência");
+        $usuarios = Usuario::factory(2)->usuarioLojista()->create();
+        Carteira::factory(2)->create();
+        $action = resolve(\App\Domain\Transferencia\Actions\ProcessarTransferenciaAction::class);
+        $transferenciaMock = new \App\Domain\Transferencia\DataTransferObjects\TransferenciaData(
+            usuario_origem:  $usuarios->first()->id,
+            usuario_destino: $usuarios->last()->id,
+            valor: 99
+        );
+        $action->execute($transferenciaMock);
     }
 }
